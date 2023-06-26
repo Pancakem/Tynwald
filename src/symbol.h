9a3dec3bfcb80d95cb29480c66b640923d68d7c1
Jerry Nyoike <jngugi31@gmail.com>
2023-06-26 06:41:55 -0400

    define symbol data structures to enable name analysis

---
+++ src/symbol.h
@@ -0,0 +1,40 @@
+#ifndef SYMBOL_H
+#define SYMBOL_H
+
+typedef enum {
+  VOID,
+  INT,
+  CHAR,
+  FLOAT,
+  POINTER
+} base_type;
+
+typedef struct {
+  base_type pointer;
+  base_type type;
+} pointer_type;
+
+
+typedef union {
+  base_type b_type;
+  pointer_type p_type;
+} type;
+
+typedef enum {
+  VAR_SYMBOL,
+  FUN_SYMBOL,
+} symbol_type;
+
+typedef struct {
+  symbol_type symbol_type;
+  type type;
+  char* name;
+} var_symbol;
+
+typedef struct {
+  symbol_type symbol_type;
+  type type;
+  char* name;
+  var_symbol* args;
+} fun_symbol;
+#endif
