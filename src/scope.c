aabe290bedba8d6b055cbe93bf1610357f5c435c
Jerry Nyoike <jngugi31@gmail.com>
2023-11-05 07:04:55 -0500

    Change 1 => remove type casts from the malloc calls as this is not required in C (https://stackoverflow.com/questions/605845/should-i-cast-the-result-of-malloc) Change 2 => only initialize the parent_scope when a child is created to avoid wasting space Change 3 => make the call to ht_find in extend_scope and return either an error or success based on the return of ht_find Change 4 => define a function to look up a symbol in the scope

--- src/scope.c
+++ src/scope.c
@@ -5,11 +5,10 @@
 /**
  * @brief: Initializes a new scope and returns a pointer to it.
  */
-scope* init_scope() {
-  scope* sc = (scope*) malloc(sizeof(scope));
-
+scope* init_scope(void) {
+  scope* sc = malloc(sizeof(scope));
+  
   sc->level = 0;
-  sc->parent_scope = (struct scope*) malloc(sizeof(scope));
   sc->table = ht_new((eq_cb_t)strcmp, (hash_cb_t) symbol_hash);
 
   return sc;
@@ -23,9 +22,10 @@ scope* init_scope() {
  */
 scope* create_scope(scope* parent_scope) {
   scope* child = init_scope();
-  child->parent_scope = (struct scope*) parent_scope;
+  child->parent_scope = malloc(sizeof (scope*));
+  child->parent_scope = parent_scope;
   child->level = parent_scope->level + 1;
-
+  
   return child;
 }
 
@@ -36,13 +36,41 @@ scope* create_scope(scope* parent_scope) {
  * @param scope
  * @param sym
  * @param int
+ * @return int 
  */
 int extend_scope(scope* scope, symbol sym) {
   char* key = sym.type == VAR_SYMBOL ? sym.val.var.name : sym.val.fun.name;
-  symbol* res = (symbol*) ht_insert(scope->table,
-				   key,
-				   (void*) &sym);
 
-  return strcmp(key,
-		res->type == VAR_SYMBOL ? res->val.var.name : res->val.fun.name);
+  if (ht_find(scope->table, &sym) == HT_NOTFOUND) {
+    symbol* res = (symbol*) ht_insert(scope->table,
+				      key,
+				      (void*) &sym);
+    return strcmp(key,
+		  res->type == VAR_SYMBOL ? res->val.var.name : res->val.fun.name);
+  }
+
+  return ERR_SYMBOL_IN_SCOPE;
+}
+
+/**
+ * @brief checks if a symbol is in scope if not found in the current
+ *        scope, checks the parent scope. If there is no parent scope
+ *        returns ERR_SYMBOL_NOT_FOUND
+ *
+ * @param scope*
+ * @param symbol
+ * @return int
+ */
+int in_scope(scope* scope, symbol sym) {
+  symbol* res = ht_find(scope->table, &sym);
+
+  if (res == HT_NOTFOUND) {
+    if (scope->level == 0) {
+      return ERR_SYMBOL_NOT_IN_SCOPE;
+    }
+
+    return in_scope(scope->parent_scope, sym);
+  }
+
+  return 1;  
 }
